8eb0a2b2667382a36906939f5825a50ae3d5adfa
Neha Muskan <88700612+NehaMuskan@users.noreply.github.com>
2022-12-16 11:34:11 +0530

    Q6-g

--- Assignment 3/Q6-g.py
+++ Assignment 3/Q6-g.py
@@ -1,10 +1,8 @@
+
 def func(n):
     for i in range(1, n + 1):
        for j in range(1,n+1):
-          if(i==1 or i==n or j==1 or j==n):
-              print("*",end=" ")
-          else:
-              print(" ",end=" ")
+           print("*",end=" ")
            
        print()
         
