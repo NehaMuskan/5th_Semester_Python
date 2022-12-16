93ad5c8743c55b82e8ebb00e413b0941bdaf8bb4
Neha Muskan <88700612+NehaMuskan@users.noreply.github.com>
2022-12-16 11:42:28 +0530

    Q6-h

---
+++ Assignment 3/Q6-h.py
@@ -0,0 +1,14 @@
+def func(n):
+    for i in range(0, n ):
+       for j in range(n-i,0,-1):
+           print(" ",end="")
+       for j in range(0,i):
+           print("*",end="")
+       for j in range(1,i):
+           print("*",end="")
+           
+       print()
+        
+        
+n=int(input())
+func(n)
